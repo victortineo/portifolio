c85aa9d1350c2ef84230885d96b5e32481bb8478
Victor Tineo <victor.tineo141@gmail.com>
2019-04-11 16:43:12 -0300

    Updates

---
+++ precache-manifest.9871a4f6384bbcb4bcbd309b706d3dfc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dbe5aec3e1ef5ab05f42823d30e3a17",
+    "url": "/portifolio/static/media/funil.1dbe5aec.jpg"
+  },
+  {
+    "revision": "871b6fbbe255f9ef62bb",
+    "url": "/portifolio/static/css/main.1b0a35b9.chunk.css"
+  },
+  {
+    "revision": "91a74ff0904045e04265",
+    "url": "/portifolio/static/js/runtime~main.e9cc7322.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portifolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "2a0f43b082db7510bed7",
+    "url": "/portifolio/static/js/2.573070e1.chunk.js"
+  },
+  {
+    "revision": "1cf34a907c8fbf78d8c97d80469f3371",
+    "url": "/portifolio/static/media/mestre.1cf34a90.jpg"
+  },
+  {
+    "revision": "dff10bb650ab77162642b3681d0e3072",
+    "url": "/portifolio/static/media/mfmti.dff10bb6.jpg"
+  },
+  {
+    "revision": "871b6fbbe255f9ef62bb",
+    "url": "/portifolio/static/js/main.2704dae8.chunk.js"
+  },
+  {
+    "revision": "b544991473748aba8b9e0aaf7510a2c9",
+    "url": "/portifolio/static/media/senai.b5449914.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portifolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portifolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portifolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "2a0f43b082db7510bed7",
+    "url": "/portifolio/static/css/2.e66f2a8c.chunk.css"
+  },
+  {
+    "revision": "a7b33d9197475e5bce248c2ce292d90f",
+    "url": "/portifolio/index.html"
+  }
+];
